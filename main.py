6a11f630e8f86a2306449fc277acb86f71cfd4b6
KottaNiranjan <95270261+KottaNiranjan@users.noreply.github.com>
2024-02-01 10:35:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import google.generativeai as gen_ai
 # GOOGLE_API_KEY="AIzaSyDj6ZsZUr9TZHUmAR2YXGIPIEspiw2AeYA"
 
 st.set_page_config(
-    page_title="Chat with Gemini Pro",
+    page_title="Chat with Niranjan"
     page_icon=":brain:",
     layout="centered"
 )
@@ -22,13 +22,13 @@ def translate_role_for_streamlit(user_role):
 if 'chat_session' not in st.session_state:
     st.session_state.chat_session=model.start_chat(history=[])
 
-st.title("Gemini Pro - ChatBot")
+st.title("Niranjan - ChatBot")
 
 for message in st.session_state.chat_session.history:
     with st.chat_message(translate_role_for_streamlit(message.role)):
         st.markdown(message.parts[0].text)
         
-user_prompt=st.chat_input("Ask Gemini-Pro")
+user_prompt=st.chat_input("Ask Niranjan")
 
 if user_prompt:
     st.chat_message("user").markdown(user_prompt)
